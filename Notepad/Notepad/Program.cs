eb41d90a82ef4167fc8aa987b5a5d685b1b10845
elifzehraunal <elifzehraunal0@gmail.com>
2025-11-17 22:18:17 +0300

    Add files via upload

---
+++ Notepad/Notepad/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Notepad
+{
+    static class Program
+    {
+        [STAThread]
+        static void Main()
+        {
+            Application.Run(new Form1());
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.EnableVisualStyles();
+
+        }
+    }
+}
